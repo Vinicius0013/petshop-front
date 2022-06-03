a236078394a2161d7e442f7741d9a75dae04a6e4
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2022-06-03 18:48:53 -0300

    Organizando o projeto, criação da estrutura de Master Pages, configurando arquivo de rotas e utilizando também o active link.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SignupPageComponent } from './pages/account/signup-page/signup-page.com
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FramePageComponent } from './pages/master/frame.page';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
     SignupPageComponent,
     PetsPageComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    AppRoutingModule
+    // RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
